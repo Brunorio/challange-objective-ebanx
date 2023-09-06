4f2ee28a3a4f71aa4e11da5f67bb0bc1f1b3de37
Bruno Santos <bruh_santos@live.com>
2023-09-06 13:11:30 -0300

    test: :test_tube: Account unit test to increase and decrease balance

--- app/tests/Unit/AccountTest.php
+++ app/tests/Unit/AccountTest.php
@@ -12,5 +12,40 @@ class AccountTest extends TestCase {
     $this->expectException(\InvalidArgumentException::class);
     $account = new Account("C1", -10);
   }
+
+  public function testshouldIncreaseBalance(){
+    $account = new Account("C1", 10);
+    $account->increaseBalance(20);
+
+    $this->assertEquals($account->getBalance(), 30);
+  }
+
+  public function testshouldThrowInvalidArgumentExceptionWhenIncreaseBalanceWithNegativeValue(){
+    $this->expectException(\InvalidArgumentException::class);
+    $account = new Account("C1", 50);
+    $account->increaseBalance(-25);
+  }
+
+  public function testshouldDecreaseBalance(){
+    $account = new Account("C1", 10);
+    $account->decreaseBalance(10);
+
+    $this->assertEquals($account->getBalance(), 0);
+  }
+
+  public function testshouldThrowInvalidArgumentExceptionWhenDecreaseBalanceWithNegativeValue(){
+    $this->expectException(\InvalidArgumentException::class);
+    $account = new Account("C1", 50);
+    $account->decreaseBalance(-25);
+  }
+
+  public function testshouldThrowExceptionWhenBalanceIsInsufficient(){
+    $this->expectException(\Exception::class);
+    $account = new Account("C1", 10);
+    $account->decreaseBalance(15);
+  }
+
+
+  
  
 }
