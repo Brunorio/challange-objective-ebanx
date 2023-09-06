f1173c03da28c2be733a281c24966dcce9eb2ee5
Bruno Santos <bruh_santos@live.com>
2023-09-06 14:55:19 -0300

    refactor: :recycle: rename main source

--- src/domain/entity/Account.php
+++ src/domain/entity/Account.php
@@ -4,7 +4,7 @@ class Account {
   
   public function __construct(
     private string $id,
-    private string $balance
+    private float $balance
   ) {
     $this->validate();
   }
@@ -15,6 +15,28 @@ class Account {
 
     if($this->balance < 0)
       throw new InvalidArgumentException("Balance cannot be negative");
+  }
 
+  public function increaseBalance(float $value){
+    if($value < 0)
+      throw new InvalidArgumentException("Value cannot be negative");
+    $this->balance += $value;
+  }
+
+  public function decreaseBalance(float $value){
+    if($value < 0)
+      throw new InvalidArgumentException("Value cannot be negative");
+    if($this->balance - $value < 0)
+      throw new Exception("Insufficient balance");
+      
+    $this->balance -= $value;
+  }
+
+  public function getId(){
+    return $this->id;
+  }
+
+  public function getBalance(){
+    return $this->balance;
   }
 }
