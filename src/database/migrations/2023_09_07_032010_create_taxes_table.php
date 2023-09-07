1ec75c05f4c37d4fa66f000323aaff2dcf8a34f0
Bruno Santos <bruh_santos@live.com>
2023-09-07 06:04:59 -0300

    feat: :sparkles: Create migrations

--- src/database/migrations/2023_09_07_032010_create_taxes_table.php
+++ src/database/migrations/2023_09_07_032010_create_taxes_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('taxes', function (Blueprint $table) {
+            $table->string("id", 1)->unique();
+            $table->string("name")->nullable(false);
+            $table->decimal("tax", 8, 2, true);
         });
     }
 
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('taxes');
     }
 };
